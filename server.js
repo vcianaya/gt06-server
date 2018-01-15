8c6ccaa1f5d5004a446b922b10f61f39ed80a4db
unknown <vcianya>
2018-01-15 15:45:57 -0400

    cambios

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 'use-strict'
+EventEmitter = require('events').EventEmitter;
 net = require('net');
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
 //TCP SERVER
+var gps_event = new EventEmitter();
 net.createServer(function (connection) {
     connection.on('data', function (data) {
         console.log("TCP-SERVER-ON-LINE");
-        console.log(data);        
-    });
-    
+        console.log(data.toString('hex'));
+        gps_event.emit("someEvent");
+    });        
 }).listen(5000);
 //HTTP SERVER
 const app = express();
@@ -27,4 +29,8 @@ io.on('connection', (socket)=>{
     socket.emit('hello',{
         nobre: 'Victor Anaya'
     });
+});
+
+gps_event.on("someEvent", function () {
+    console.log("event has occured");
 });
