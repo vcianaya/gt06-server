a095f12f5d2d97c05839d8b9e04e59a8e912f741
unknown <vcianya>
2018-01-15 15:14:00 -0400

    Inicializando proyecto

---
+++ server.js
@@ -0,0 +1,29 @@
+'use-strict'
+net = require('net');
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+//TCP SERVER
+net.createServer(function (connection) {
+    connection.on('data', function (data) {
+        console.log("TCP-SERVER-ON-LINE");
+    });
+    
+}).listen(5000);
+//HTTP SERVER
+const app = express();
+app.get('/', (req, res)=>{
+    res.send("INICIADO")
+});
+
+const server = http.Server(app);
+server.listen(3000);
+
+const io = socketIo(server);
+
+io.on('connection', (socket)=>{
+    console.log('Algien se a connectado');
+    socket.emit('hello',{
+        nobre: 'Victor Anaya'
+    });
+});
