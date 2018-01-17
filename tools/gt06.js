bb619578f89cad617b9358b6a2310afc274c3a57
unknown <vcianya>
2018-01-17 10:41:53 -0400

    adA

--- tools/gt06.js
+++ tools/gt06.js
@@ -12,22 +12,22 @@ exports.parse_data = (data)=>{
             parts.device_id = data.substr(8, 16);            
             parts.action = 'login_request';
         } else if (parts.protocal_id == '12') {
-            parts.device_id = '';
+            parts.device_id = data.substr(8, 16);
             parts.data = data.substr(8, parts.length * 2);            
             parts.action = 'ping';
         } else if (parts.protocal_id == '13') {
-            parts.device_id = '';            
+            parts.device_id = data.substr(8, 16);            
             parts.action = 'heartbeat';
         } else if (parts.protocal_id == '16' || parts.protocal_id == '18') {
-            parts.device_id = '';
+            parts.device_id = data.substr(8, 16);
             parts.data = data.substr(8, parts.length * 2);            
             parts.action = 'alert';
         } else {
-            parts.device_id = '';            
+            parts.device_id = data.substr(8, 16);            
             parts.action = 'noop';
         }
     } else {
-        parts.device_id = '';        
+        parts.device_id = '';
         parts.action = 'noop';
     }
     return parts;
